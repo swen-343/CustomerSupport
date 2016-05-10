b9e809543051ce53d9aafd6bf1a83c2801a135f9
Daniel <dds2130@rit.edu>
2016-05-10 01:17:49 -0400

    users and login scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   resources :tickets
   resources :customers
   get '/tickets/new/:customer_id', to: 'tickets#new', as: 'new_ticket_customer'
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507232913) do
+ActiveRecord::Schema.define(version: 20160510050732) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
     t.integer  "phone"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer  "status_id"
+    t.integer  "customer_id"
+    t.datetime "closed_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tickets", ["customer_id"], name: "index_tickets_on_customer_id"
+  add_index "tickets", ["status_id"], name: "index_tickets_on_status_id"
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
